938e83bb5df25f517cc3af830ebcc46c1660986f
harsh589459 <hs589459@gmail.com>
2023-07-09 15:57:30 +0530

    first-commit

---
+++ dom.js
@@ -0,0 +1,34 @@
+//shows all properties and methods inside that document or examine the document object
+// console.dir(document)
+
+// console.log(document.domain)
+// console.log(document.URL)
+// console.log(document.title)
+// document.title=123;
+// console.log(document.doctype)
+// console.log(document.head)
+// console.log(document.body)
+// console.log(document.all)
+// console.log(document.all[10])
+// document.all[10].textContent="kdjfldjfl"
+// console.log(document.forms[0])
+// console.log(document.links)
+
+// console.log(document.images)
+
+
+
+
+//GET ELEMENT BY ID
+// console.log(document.getElementById('header-title'));
+// let headerTitle = document.getElementById('header-title');
+// let header = document.getElementById('main-header');
+console.log(headerTitle);
+// headerTitle.textContent='Hello';
+// headerTitle.innerText="goodbye";
+// console.log(headerTitle.textContent);
+// headerTitle.innerHTML='<h3>Hello</h3>'
+// header.style.borderBottom = '3px solid black';
+
+
+//GET ELEMENTS BY CLASSNAME
