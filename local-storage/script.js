90ff72cd04ddf98f0fca6487f3abf37e6885bf1c
harsh589459 <hs589459@gmail.com>
2023-07-10 14:06:12 +0530

    assignment 11

--- local-storage/script.js
+++ local-storage/script.js
@@ -8,7 +8,25 @@ function submit(e) {
   
     localStorage.setItem('name', nameValue);
     localStorage.setItem('email', emailValue);
+    console.log(localStorage.getItem('name'));
   }
 
   let submitBtn = document.getElementById('submit');
   submitBtn.addEventListener('click',submit);
+
+
+//----------------------------------------------//
+
+
+
+  let myObj={
+    name:"domenic",
+    age:56
+  }
+
+  let myObj_serialized=JSON.stringify(myObj);
+  localStorage.setItem("myObj",myObj_serialized);
+  console.log(localStorage.getItem("myObj"));
+
+  let myObj_deserialized=JSON.parse(localStorage.getItem("myObj"));
+  console.log(myObj_deserialized);
