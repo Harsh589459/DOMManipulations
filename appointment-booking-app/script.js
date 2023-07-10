3efe0c7d00ae33990217746e47389f2140cc327d
harsh589459 <hs589459@gmail.com>
2023-07-10 14:34:18 +0530

    edit-button-added

--- appointment-booking-app/script.js
+++ appointment-booking-app/script.js
@@ -22,32 +22,6 @@ function saveBooking() {
     displayBookings();
   }
 
-  function displayBookings() {
-    var bookings = localStorage.getItem("bookings");
-    if (bookings) {
-      bookings = JSON.parse(bookings);
-
-      var bookingsList = document.getElementById("bookings-list");
-      bookingsList.innerHTML = "";
-      for (var i = 0; i < bookings.length; i++) {
-        var booking = bookings[i];
-        var listItem = document.createElement("li");
-        listItem.textContent =
-          booking.name + " | " + booking.email + " | " + booking.phone;
-
-        var deleteButton = document.createElement("button");
-        deleteButton.textContent = "Delete";
-        deleteButton.setAttribute("data-index", i);
-        deleteButton.addEventListener("click", function () {
-          var index = parseInt(this.getAttribute("data-index"));
-          deleteBooking(index);
-        });
-
-        listItem.appendChild(deleteButton);
-        bookingsList.appendChild(listItem);
-      }
-    }
-  }
   function deleteBooking(index) {
     var bookings = localStorage.getItem("bookings");
     if (bookings) {
@@ -57,5 +31,56 @@ function saveBooking() {
       localStorage.setItem("bookings", JSON.stringify(bookings));
 
       displayBookings();
+    }
+  }
+
+  function editBooking(index) {
+    var bookings = localStorage.getItem("bookings");
+    if (bookings) {
+      bookings = JSON.parse(bookings);
+
+      var booking = bookings[index];
+      document.getElementById("name").value = booking.name;
+      document.getElementById("email").value = booking.email;
+      document.getElementById("phone").value = booking.phone;
+
+      deleteBooking(index);
+    }
+  }
+
+  function displayBookings() {
+    var bookings = localStorage.getItem("bookings");
+    if (bookings) {
+      bookings = JSON.parse(bookings);
+
+      var bookingsList = document.getElementById("bookings-list");
+      bookingsList.innerHTML = "";
+
+      for (var i = 0; i < bookings.length; i++) {
+        var booking = bookings[i];
+        var listItem = document.createElement("li");
+        listItem.textContent =
+          booking.name + " | " + booking.email + " | " + booking.phone;
+
+        var editButton = document.createElement("button");
+        editButton.textContent = "Edit";
+        editButton.setAttribute("data-index", i);
+        editButton.addEventListener("click", function () {
+          var index = parseInt(this.getAttribute("data-index"));
+          editBooking(index);
+        });
+
+        var deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.setAttribute("data-index", i);
+        deleteButton.addEventListener("click", function () {
+          var index = parseInt(this.getAttribute("data-index"));
+          deleteBooking(index);
+        });
+
+        listItem.appendChild(editButton);
+        listItem.appendChild(deleteButton);
+        bookingsList.appendChild(listItem);
+      }
     }
   }
